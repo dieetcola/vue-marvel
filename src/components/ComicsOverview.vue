50d6917f591d94b35e2071f6749f9cbde412ec5e
Irakli Svanidze <svanidzeee@gmail.com>
2024-07-02 17:07:44 +0300

    implemented routes

--- src/components/ComicsOverview.vue
+++ src/components/ComicsOverview.vue
@@ -3,6 +3,8 @@ import { ref, onMounted } from "vue";
 import type { Ref } from "vue";
 import { useComics } from "@/composables/marvelApi";
 import type { Comic } from "@/types/marvel";
+import LoadingIndicator from "./LoadingIndicator.vue";
+import ComicCard from "./ComicCard.vue";
 
 const isLoading: Ref<boolean> = ref(false);
 const data: Ref<Comic[] | undefined> = ref();
@@ -23,10 +25,10 @@ onMounted(async () => {
 
 <template>
   <div>
-    <div v-if="isLoading"><p>Loading comics…</p></div>
+    <LoadingIndicator v-if="isLoading" text="Loading comics..." />
     <div v-if="data && !isLoading">
       <div class="grid grid-flow-row grid-cols-1 gap-4 md:grid-cols-2 lg:grid-cols-4">
-        <div :key="comic.id" v-for="comic in data">{{ comic.title }}</div>
+        <ComicCard :comic="comic" :key="comic.id" v-for="comic in data"></ComicCard>
       </div>
     </div>
   </div>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,8 +1,10 @@
 <script setup lang="ts">
+import ComicsOverview from "../components/ComicsOverview.vue";
 </script>
 
 <template>
   <main>
+    <ComicsOverview />
     1
   </main>
 </template>
