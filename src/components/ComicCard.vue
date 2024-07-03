1470ebb74c0eca8379e5a18e95685fba2faa2bc7
Irakli Svanidze <svanidzeee@gmail.com>
2024-07-03 14:16:06 +0300

    implemented unknown routes

--- src/components/ComicCard.vue
+++ src/components/ComicCard.vue
@@ -1,6 +1,6 @@
 <script setup lang="ts">
 import { computed } from "vue";
-import type { Comic } from "@/types/marvel";
+import type { Comic } from "../types/marvel.ts";
 import CardView from "../views/CardView.vue";
 
 const props = defineProps<{
--- src/router/index.ts
+++ src/router/index.ts
@@ -1,20 +1,25 @@
-import { createRouter, createWebHistory } from 'vue-router'
-import HomeView from '../views/HomeView.vue'
+import { createRouter, createWebHistory } from "vue-router";
+import HomeView from "../views/HomeView.vue";
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
     {
-      path: '/:page?',
-      name: 'home',
-      component: HomeView
+      path: "/:page?",
+      name: "home",
+      component: HomeView,
     },
     {
-      path: '/search',
-      name: 'search',
-      component: () => import('../views/SearchView.vue')
-    }
-  ]
-})
+      path: "/search",
+      name: "search",
+      component: () => import("../views/SearchView.vue"),
+    },
+    {
+      path: "/error",
+      name: "error",
+      component: () => import("../views/ErrorView.vue"),
+    },
+  ],
+});
 
-export default router
+export default router;
